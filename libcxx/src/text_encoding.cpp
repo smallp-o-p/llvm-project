c9cfc61ae1afc33d7abdf30abefd8cc8073f6c44
William Tran-Viet <wtranviet@proton.me>
2025-04-11 21:31:56 -0400

    Add text_encoding files

---
+++ libcxx/src/text_encoding.cpp
@@ -0,0 +1,14 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <text_encoding> 
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+
+
+_LIBCPP_END_NAMESPACE_STD 
