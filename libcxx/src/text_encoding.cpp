3efdc5ed5aad69b490b5ce4eb5bb15506acc95fd
William Tran-Viet <wtranviet@proton.me>
2025-04-18 20:39:39 -0400

    Add locale support for text_encoding

--- libcxx/src/text_encoding.cpp
+++ libcxx/src/text_encoding.cpp
@@ -7,8 +7,19 @@
 //===----------------------------------------------------------------------===//
 
 #include <text_encoding> 
+#include <locale> 
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
+text_encoding locale::encoding() const {
+  auto __locale_name = name(); 
+  if(__locale_name.size() == 1){
+    if(__locale_name[0] == 'C')
+      return text_encoding(text_encoding::ASCII);
+    if(__locale_name[0] == '*')
+      return text_encoding(); 
+  }
 
+  return text_encoding(name().c_str());
+}
 _LIBCPP_END_NAMESPACE_STD 
