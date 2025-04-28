7821b8e23f0b4050c4f78338b771cbcb92eec7d2
William Tran-Viet <wtranviet@proton.me>
2025-04-28 18:53:56 -0400

    Add operator== test cases

---
+++ libcxx/test/std/utilities/text_encoding/text_encoding.eq/equal.id.pass.cpp
@@ -0,0 +1,54 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <text_encoding>
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20, c++23
+
+// class text_encoding
+
+// text_encoding operator==(const text_encoding&, id) _NOEXCEPT 
+
+// Concerns:
+// 1. operator==(const text_encoding&, id) must be noexcept
+// 2. operator==(const text_encoding&, id) returns true if mib() is equal to the id
+// 3. operator==(const text_encoding&, id) returns false if mib() is not equal to the id
+
+#include <cassert>
+#include <text_encoding>
+#include <type_traits>
+
+#include "test_macros.h"
+#include "test_text_encoding.h"
+
+using id = std::text_encoding::id;
+
+int main() {
+
+  { // 1
+    auto te = std::text_encoding();
+    ASSERT_NOEXCEPT(te == id::UTF8);
+  }
+
+  { // 2
+    auto te = std::text_encoding(id::UTF8);
+    assert(te == id::UTF8);
+  }
+
+  { // 2.1
+    auto te = std::text_encoding(id::other);
+    assert(te == id::other);
+  }
+
+  { // 3
+    auto te = std::text_encoding(id::UTF8);
+    assert(!(te == id::UTF16));
+  }
+
+
+}
