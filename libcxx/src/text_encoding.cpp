7a8dbfd92fb9e4fabe76178e3cd0945afe941a90
William Tran-Viet <wtranviet@proton.me>
2025-04-30 17:28:12 -0400

    format source file

--- libcxx/src/text_encoding.cpp
+++ libcxx/src/text_encoding.cpp
@@ -25,21 +25,21 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 #if __CHAR_BIT__ == 8
 
 text_encoding text_encoding::environment() {
-    auto __make_locale = [](const char* __name) {
-      text_encoding __enc{};
-      if (auto __loc = __locale::__newlocale(LC_CTYPE_MASK, __name, static_cast<locale_t>(0))) {
-        if (const char* __codeset = nl_langinfo_l(CODESET, __loc)) {
-          string_view __s(__codeset);
-          if (__s.size() < max_name_length)
-            __enc = text_encoding(__s);
-        }
-        __locale::__freelocale(__loc);
+  auto __make_locale = [](const char* __name) {
+    text_encoding __enc{};
+    if (auto __loc = __locale::__newlocale(LC_CTYPE_MASK, __name, static_cast<locale_t>(0))) {
+      if (const char* __codeset = nl_langinfo_l(CODESET, __loc)) {
+        string_view __s(__codeset);
+        if (__s.size() < max_name_length)
+          __enc = text_encoding(__s);
       }
-      return __enc;
-    };
+      __locale::__freelocale(__loc);
+    }
+    return __enc;
+  };
 
-    return __make_locale("");
-  }
+  return __make_locale("");
+}
 
 # endif  // __CHAR_BIT__ == 8
 
