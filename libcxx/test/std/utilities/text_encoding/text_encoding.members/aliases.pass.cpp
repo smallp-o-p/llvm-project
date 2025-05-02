63f7ceddff288796220bc20603ead1d11fe2105d
William Tran-Viet <wtranviet@proton.me>
2025-05-02 11:26:14 -0400

    Add aliases test

---
+++ libcxx/test/std/utilities/text_encoding/text_encoding.members/aliases.pass.cpp
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <text_encoding>
+
+// REQUIRES: std-at-least-c++26
+// REQUIRES: locale.en_US.UTF-8
+
+// UNSUPPORTED: no-localization
+
+// class text_encoding
+
+// text_encoding text_encoding::environment(); 
+
+// Concerns:
+// 1. todo 
+
+#include <cassert>
+#include <cstdlib>
+#include <string_view>
+#include <text_encoding>
+
+#include "platform_support.h" 
+#include "test_macros.h"
+#include "test_text_encoding.h"
+
+int main() {
+
+  
+}
